a96a2b3716a4c694f0ebc2ade94d49a57de49b50
Aleksandar  Stoisavljevic <stojke@stojke.local>
2023-05-06 00:05:01 +0200

    signup login pages

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG=MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        EditText username=findViewById(R.id.editTextUserName);
+        EditText userpassword=findViewById(R.id.editTextPassword);
+
+        String usernamestr=username.getText().toString();
+        String userpasswordstr=userpassword.getText().toString();
+
+        Log.i(LOG_TAG,"Bejeletnkezet "+usernamestr+userpasswordstr);
+
+    }
+
+    public void register(View view) {
     }
 }
---
+++ app/src/main/java/com/example/myapplication/register.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void cancel(View view) {
+    }
+
+    public void signup(View view) {
+    }
+}
